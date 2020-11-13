e5a6d6fb58ceb87d233172d8898712175b1cc0c6
William Di Pasquale <wdp@dipasqualew.com>
2020-11-13 09:56:52 +0000

    feat: Add JSdoc plugin configuration to `eslint-config`
    The JSdoc plugin should be activated by `mixins/jsdoc.js`
    
    Most of the rules have been inherited from `plugin:jsdoc/recommended`
    and therefore they will raise warnings rather than error. I think this
    is preferable than setting all the options manually.
    
    A few rules have been disabled or options have been updated. Comments
    will explain the rationale behind each override.
    
    See:
    - [ENG-33]
    - [ENG-43]

---
+++ mixins/jsdoc.js
@@ -0,0 +1,51 @@
+module.exports = {
+  "plugins": [
+    "jsdoc",
+  ],
+  "extends": [
+    "plugin:jsdoc/recommended",
+  ],
+  "rules": {
+    // Enable JSdoc checks
+    "jsdoc/require-jsdoc": [2, {
+      checkGetters: true,
+      checkSetters: true,
+      publicOnly: true,
+      checkConstructors: false,
+      require: {
+        ArrowFunctionExpression: true,
+        ClassDeclaration: true,
+        ClassExpression: true,
+        FunctionDeclaration: true,
+        FunctionExpression: true,
+        MethodDefinition: true,
+      },
+    }],
+    // The fixer from `jsdoc/require-jsdoc`
+    // will create an automatic jsdoc block
+    // which is helpful, but still incomplete.
+    // We want to flag those
+    "jsdoc/require-description": 2,
+
+    // Parameters and returns may be self explaining
+    "jsdoc/require-param-description": "off",
+    "jsdoc/require-returns-description": "off",
+
+    // Adding types to JSdoc is a chore
+    // we should be using TypeScript instead
+    "jsdoc/require-param-type": "off",
+    "jsdoc/no-undefined-types": "off",
+
+    // Requiring returns really means
+    // requiring types, see above
+    "jsdoc/require-returns": "off",
+
+    // Add custom JSdoc tags
+    "jsdoc/check-tag-names": [2, {
+      definedTags: [
+        "mermaid"
+      ],
+    }],
+
+  }
+};
