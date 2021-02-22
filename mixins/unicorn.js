2d394a0d0fb9cb955f526c8cccaf059e65075dd8
William Di Pasquale <wdp@dipasqualew.com>
2021-02-22 11:14:04 +0000

    fix: Turn off `unicorn/prefer-ternary` (#28)
    Ternary operators almost always make the code harder to understand and
    maintain.

--- mixins/unicorn.js
+++ mixins/unicorn.js
@@ -10,5 +10,6 @@ module.exports = {
     "unicorn/no-null": "off",
     "unicorn/no-array-for-each": "off",
     "unicorn/prevent-abbreviations": "off",
+    "unicorn/prefer-ternary": "off",
   },
 };
