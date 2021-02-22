5ba4efc32b6ddcacd42ed6bf6e0d7a0c5e485656
William Di Pasquale <wdp@dipasqualew.com>
2021-02-22 16:03:15 +0000

    fix: Disable sonarjs/no-duplicate-string in tests
    In tests it may be helpful to just repeat a string
    instead of making very different tests dependent
    on each other

--- mixins/jest.js
+++ mixins/jest.js
@@ -7,5 +7,10 @@ module.exports = {
     "func-names": "off",
     "no-console": "off",
     "no-unused-expressions": "off",
+
+    // In tests it may be helpful to just repeat a string
+    // instead of making very different tests dependent
+    // on each other
+    "sonarjs/no-duplicate-string": "off",
   },
 }
