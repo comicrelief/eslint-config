b86535ce915a589d5e6bf92698c7fd38a76c0ec3
Seb Aebischer <s.aebischer@comicrelief.com>
2021-08-10 09:44:51 +0100

    fix: Add more extension rules for TypeScript

--- mixins/ts.js
+++ mixins/ts.js
@@ -10,14 +10,18 @@ module.exports = {
   "rules": {
     "@typescript-eslint/no-unused-vars": ["error", { "argsIgnorePattern": "^_" }],
     // these rules can report incorrect errors or crash in TypeScript files
+    "comma-dangle": "off",
     "semi": "off",
     "no-shadow": "off",
+    "no-useless-constructor": "off",
     "indent": "off",
     "import/no-webpack-loader-syntax": "off",
     "import/no-useless-path-segments": "off",
     // these are the replacement "extension rules" from @typescript-eslint
+    "@typescript-eslint/comma-dangle": ["error", "always-multiline"],
     "@typescript-eslint/semi": "error",
     "@typescript-eslint/no-shadow": "error",
+    "@typescript-eslint/no-useless-constructor": "error",
     "@typescript-eslint/indent": ["error", 2, { "SwitchCase": 0 }],
   },
 }
