cc7f03ce8302bf9cad9d6480b3d5c78328ab62bb
Seb Aebischer <49983670+seb-cr@users.noreply.github.com>
2021-10-22 14:19:52 +0100

    fix: Add more extension rules for TypeScript (#31)
    * fix: Add more extension rules for TypeScript
    
    * Reorder alphabetically and add more comments

--- mixins/ts.js
+++ mixins/ts.js
@@ -8,16 +8,22 @@ module.exports = {
     "plugin:@typescript-eslint/recommended"
   ],
   "rules": {
+    // allow unused arguments if prefixed with an underscore
     "@typescript-eslint/no-unused-vars": ["error", { "argsIgnorePattern": "^_" }],
-    // these rules can report incorrect errors or crash in TypeScript files
-    "semi": "off",
-    "no-shadow": "off",
-    "indent": "off",
+    // these rules can crash in TypeScript files
     "import/no-webpack-loader-syntax": "off",
     "import/no-useless-path-segments": "off",
+    // these rules can report incorrect errors in TypeScript files
+    "comma-dangle": "off",
+    "indent": "off",
+    "no-shadow": "off",
+    "no-useless-constructor": "off",
+    "semi": "off",
     // these are the replacement "extension rules" from @typescript-eslint
-    "@typescript-eslint/semi": "error",
-    "@typescript-eslint/no-shadow": "error",
+    "@typescript-eslint/comma-dangle": ["error", "always-multiline"],
     "@typescript-eslint/indent": ["error", 2, { "SwitchCase": 0 }],
+    "@typescript-eslint/no-shadow": "error",
+    "@typescript-eslint/no-useless-constructor": "error",
+    "@typescript-eslint/semi": "error",
   },
 }
