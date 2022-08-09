a03cfddf228c84ce38d7f62349974ac805cea359
Seb Aebischer <s.aebischer@comicrelief.com>
2022-08-09 18:04:09 +0100

    Small improvements to test runner
    - Run eslint directly instead of via yarn
    - Report result of each test
    - Heading and space around report

--- tests/test.sh
+++ tests/test.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+PATH="$PATH:$(pwd)/node_modules/bin"
+
 count=0
 passed=0
 failed=0
@@ -6,11 +8,16 @@ failed=0
 report () {
   echo
   echo
+  echo "---------- finished running $count tests ----------"
+  echo
   if [ $failed = 0 ]; then
-    echo -e "\x1b[1;32mAll tests passed ($passed/$count)\x1b[0m"
+    echo -e "\x1b[1;32mAll tests passed\x1b[0m"
+    echo
+    exit 0
   else
     echo -e "\x1b[1;32m$passed passed\x1b[0m"
     echo -e "\x1b[1;31m$failed failed\x1b[0m"
+    echo
     exit 1
   fi
 }
@@ -20,11 +27,13 @@ expect_lint_passes () {
 
   echo
   echo "---------- $dir ----------"
-  yarn eslint $dir
+  eslint $dir
   if [ $? = 0 ]; then
     passed=$(( $passed + 1 ));
+    echo -e "\x1b[32mPassed\x1b[0m"
   else
     failed=$(( $failed + 1 ));
+    echo -e "\x1b[31mFailed\x1b[0m"
   fi
   count=$(( $count + 1 ));
 }
